f9d84aaca32642fba334438e69aab045da205394
Dylan Mullins <dylanemullins92@gmail.com>
2021-02-25 20:35:18 -0600

    Added verifyPassword and password validation, updated model and view

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -19,23 +19,12 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
-        model.addAttribute("verify", verify);
-
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
         if (errors.hasErrors()) {
-
-
-            if (!user.getPassword().equals(verify)) {
-                model.addAttribute("error", "Passwords do not match");
-            }
             return "user/add";
-
-            } else if(user.getPassword().equals(verify)){
-
+            } else {
             return "user/index";
             }
-        model.addAttribute("error", "Passwords do not match");
-        return "user/add";
     }
 
 
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -20,6 +20,9 @@ public class User {
     @Size(min=6, message="Password must be at least 6 characters long.")
     private String password;
 
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
+
     public User() {
 
     }
@@ -54,5 +57,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if(password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 }
